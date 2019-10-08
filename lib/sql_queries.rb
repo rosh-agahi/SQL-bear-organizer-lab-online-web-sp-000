3f9d3cf621d073596035680cfdd10bbbd83d63ad
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-08 23:36:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,25 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  ""
+  "select name, age from bears where alive = 'true' order by age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  ""
+  "select name, age from bears order by age desc limit 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  ""
+  "select name, age from bears order by age limit 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  ""
+  "select color, count(color) from bears group by color order by count(color) desc limit 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  ""
+  "select count(name) from bears where temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  ""
+  "select * from bears where name = null;"
 end
